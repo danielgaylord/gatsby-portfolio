5caf8088cabe9ee0a472adf878753541edf73f6a
Daniel Gaylord <danielgaylord@gmail.com>
2021-09-24 00:26:43 -0400

    VS Code Settings

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -2,8 +2,7 @@ module.exports = {
     siteMetadata: {
         title: `DG's Web Portfolio & Stuff`,
         siteUrl: `https://danielgaylord.github.io`,
-        description: `This is the place I go to try new web things and share thoughts or anything else I feel ppeople might not care about`
-
+        description: `This is the place I go to try new web things and share thoughts or anything else I feel people might not care about`
     },
     pathPrefix: "/gatsby-portfolio",
     plugins: [
