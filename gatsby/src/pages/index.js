e1f857be1a8ab2c336d3995db0fac4986bf80ada
Daniel Gaylord <danielgaylord@gmail.com>
2021-10-05 18:36:17 -0400

    Update index.js

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const IndexStyles = styled.div`
-  .call-out {
-    font-size: 5rem;
-    font-weight: bold;
-  }
-
-  .dive-in {
-    & p:nth-child(2) {
-      text-align: center;
-    }
-    & p:nth-child(3) {
-      text-align: right;
-    }
-  }
 `;
 
 export default function HomePage() {
